52ba27e99cf4ca9c20675e8bbeba1740cdbb8c51
Vraj2811 <121240839+Vraj2811@users.noreply.github.com>
2023-08-21 07:49:55 +0530

    Add files via upload

---
+++ Arithmetic_ Operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 2/5;
+    cout<<a<<endl;
+
+    int b = 2.0/5;
+    cout<<b<<endl;
+
+    float c = 2.0/5;
+    cout<<c<<endl;
+
+    float d = 2/5.0;
+    cout<<d<<endl;
+
+    return 0;
+}
